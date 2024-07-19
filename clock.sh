17198ec9efd67886bb438fd207a009ea9df750f8
Brad Bierman <6399876+bierman323@users.noreply.github.com>
2024-07-19 14:55:10 -0400

    Updated the clock and the update to use ntpdate for WSL2

--- clock.sh
+++ clock.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 # This is for Ubuntu WSL. The clock gets out of whack
-sudo hwclock --hctosys
+
+if ! command -v ntpdate &> /dev/null; then
+  echo "ntpdate could not be found"
+  exit 1
+else
+  echo "Updating the time"
+  command sudo ntpdate 0.pool.ntp.org
+  echo "Time has been updated"
+fi
+
--- update.sh
+++ update.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env zsh
 
+# check to see if ntpdate is installed first
+# I need to make sure that WSL2 Ubuntu is up to date or else I can't update the OS
+if ! command -v clock.sh &> /dev/null; then
+  echo "clock.sh could not be found"
+  exit 1
+else
+  command clock.sh
+fi
+
 sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt -y autoremove
